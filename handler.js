7e4c83365993b42c4162fa3ea587e5598f16cecb
piresandre <andrepsantis@gmail.com>
2022-07-01 18:29:41 -0300

    teste

--- handler.js
+++ handler.js
@@ -6,13 +6,14 @@ module.exports.hello = async (event) => {
     body: JSON.stringify(
       {
         message: 'Hello Familyst',
-        input: event,
+        // input: event,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+
+
+
